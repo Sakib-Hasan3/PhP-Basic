34ce6a064568b94f2a7bba88f0218d8a28eb42cb
Mohammed Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2025-05-24 11:40:01 +0600

    Update logout.php

--- Php Project/CSE_STUDY_ROOM/auth/logout.php
+++ Php Project/CSE_STUDY_ROOM/auth/logout.php
@@ -2,5 +2,5 @@
 session_start();
 session_unset();
 session_destroy();
-header("Location: login.php"); // No change if login.php is in the same 'auth/' folder
+header("Location: login.php"); 
 exit();
